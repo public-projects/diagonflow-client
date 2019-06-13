a7c954033fa564d63eb41321b5845f446c0fd0a7
Sagi Kashti <projectk771@gmail.com>
2019-06-13 16:45:30 +0300

    client<->nodejs<->mongodb work no interceptors no history caching

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../chat.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ChatDialogComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   exports: [ ChatDialogComponent ],
   providers: [ChatService]

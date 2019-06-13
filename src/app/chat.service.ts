7dc7e1ea37e3bf27fc3dc9db39a4ae3499e417a4
Sagi Kashti <projectk771@gmail.com>
2019-06-13 19:01:20 +0300

    client<->nodejs<->mongodb work no interceptors no history caching

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
 // import { ApiAiClient } from 'api-ai-javascript';
-import {ApiAiClient} from 'api-ai-javascript/es6/ApiAiClient';
-import { BehaviorSubject } from 'rxjs';
-
+import { ApiAiClient } from 'api-ai-javascript/es6/ApiAiClient';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 export class Message {
-  constructor(public content: string, public sentBy: string) {}
+  constructor(public content: string, public sentBy: string) { }
 }
 
 @Injectable({
@@ -19,18 +20,34 @@ export class ChatService {
 
   conversation = new BehaviorSubject<Message[]>([]);
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  converse(msg: string) {
+  converse(msg: string): Observable<any> {
     const userMessage = new Message(msg, 'user');
     this.update(userMessage);
+    const body = {
+      question: userMessage.content
+      // question: 'how do you feel?'
+    };
+    // check question in history
 
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    }
+    this.httpClient.post('http://10.0.0.3:3000/chatbot/find', body, httpOptions).toPromise().then(
+      (res) => {
+        console.log('res =');
+        console.log(res);
+      }
+    );
     return this.client.textRequest(msg)
-               .then(res => {
-                  const speech = res.result.fulfillment.speech;
-                  const botMessage = new Message(speech, 'bot');
-                  this.update(botMessage);
-               });
+      .then(res => {
+        const speech = res.result.fulfillment.speech;
+        const botMessage = new Message(speech, 'bot');
+        this.update(botMessage);
+      });
   }
 
   update(msg: Message) {
